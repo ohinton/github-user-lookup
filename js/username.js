fe5deb1cbc5028706fa0477c0c7cad171b237287
Olivia Hinton <hinton.olivia@gmail.com>
2016-08-05 11:21:54 -0700

    display username and user location

--- js/username.js
+++ js/username.js
@@ -8,6 +8,10 @@ function Username(){
 Username.prototype.getRepos = function(username){
   $.get('https://api.github.com/users/'+ username +'?access_token=' + apiKey).then(function(response){
     var reposLink = response.repos_url;
+    var username = response.name;
+    var userLocation = response.location;
+    $('#resultName').text("Name: " + username);
+    $('#resultLocation').text("Location: " + userLocation);
 
   $.get(reposLink + "?page=1&per_page=500").then(function(response){
     response.forEach(function(repo){
@@ -18,7 +22,6 @@ Username.prototype.getRepos = function(username){
     var formattedDate = moment(createdAt).format('MMM D, YYYY @ h:mm a');
 
     $('.showResults').append("<li>" + "Name: " + "<a target='_blank' href='" + repoURL + "'>" + name + "</a>" + "<br>" + "Description: " + description + "<br>" + "Created: " + formattedDate + "</li>" + "<br>");
-    $('#resultHeader').show();
   });
       }).fail(function(error){
       $('#showResults').text(error.responseJSON.message);
